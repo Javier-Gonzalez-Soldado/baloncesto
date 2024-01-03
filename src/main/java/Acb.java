670a9d9aad3caf703f377971136fd3f5650122ce
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-03 15:34:33 +0100

    Inicio requisito 2

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -12,7 +12,25 @@ public class Acb extends HttpServlet {
         bd.abrirConexion();
     }
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        //Obtenemos el método de la petición
+        String metodo = req.getMethod();
+        
+        if (metodo.equals("POST")) {
+            // Si es un POST, procesamos los datos
+            procesarPost(req, res);
+
+        }else if(metodo.equals("DELETE")){
+            procesarDelete(req, res);
+        }
+        else {
+            // Si es un GET u otro, redirigimos a la página de inicio
+            res.sendRedirect(res.encodeRedirectURL("index.html"));
+        }
+    }
+
+    private void procesarPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession s = req.getSession(true);
         String nombreP = (String) req.getParameter("txtNombre");
         String nombre = (String) req.getParameter("R1");
@@ -27,6 +45,11 @@ public class Acb extends HttpServlet {
         s.setAttribute("nombreCliente", nombreP);
         // Llamada a la página jsp que nos da las gracias
         res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
+    }
+
+    private void procesarDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        bd.ponerVotosCero();
+        res.sendRedirect(res.encodeRedirectURL("index.html"));
     }
 
     public void destroy() {
