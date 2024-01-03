946f60452487414209e1212fd1a182f7cfc4a499
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-03 16:37:55 +0100

    Añadidos test unitarios

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -13,4 +13,46 @@ public class ModeloDatosTest {
         assertEquals(expResult, result);
         // fail("Fallo forzado.");
     }
+
+    @Test
+    public void testReiniciarVotos(){
+        System.out.println("Prueba de reiniciarVotos");
+        ModeloDatos instance = new ModeloDatos();
+        instance.reiniciarVotos();
+        // fail("Fallo forzado.");
+    }
+
+    @Test
+    public void testActualizarJugador() {
+        System.out.println("Prueba de actualizarJugador");
+        String nombre = "";
+        ModeloDatos instance = new ModeloDatos();
+        instance.actualizarJugador(nombre);
+        // fail("Fallo forzado.");
+    }
+
+    @Test
+    public void testInsertarJugador() {
+        System.out.println("Prueba de insertarJugador");
+        String nombre = "";
+        ModeloDatos instance = new ModeloDatos();
+        instance.insertarJugador(nombre);
+        // fail("Fallo forzado.");
+    }
+
+    @Test
+    public void testAbrirConexion() {
+        System.out.println("Prueba de abrirConexion");
+        ModeloDatos instance = new ModeloDatos();
+        instance.abrirConexion();
+        // fail("Fallo forzado.");
+    }
+
+    @Test
+    public void testCerrarConexion() {
+        System.out.println("Prueba de cerrarConexion");
+        ModeloDatos instance = new ModeloDatos();
+        instance.cerrarConexion();
+        // fail("Fallo forzado.");
+    }
 }
