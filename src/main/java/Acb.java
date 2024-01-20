eb05a69d01eabc46e1ccf25e7ad03bfdf2340191
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-20 20:44:15 +0100

    Inicio del REQ-2

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -14,6 +14,15 @@ public class Acb extends HttpServlet {
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         HttpSession s = req.getSession(true);
+
+        //Si se ha pulsado el botón de ver votos
+        if (req.getParameter("verVotos") != null) {
+            // Llamada a la página jsp que muestra una tabla con los votos
+            res.sendRedirect(res.encodeRedirectURL("VerVotos.jsp"));
+            return;
+        }
+
+
         String nombreP = (String) req.getParameter("txtNombre");
         String nombre = (String) req.getParameter("R1");
         if (nombre.equals("Otros")) {
