a6d96ce1b42c4748914864dd1f5a97045b5476e5
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-20 17:39:40 +0100

    Arreglando el AssertEquals

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -26,7 +26,9 @@ public class ModeloDatosTest {
 
         instance.actualizarJugador(nombre);
 
-        assertEquals(votosLlullPrev + 1, instance.getVotosJugador(nombre));
+        Integer votosLlullPost = instance.getVotosJugador(nombre);
+
+        assertEquals(votosLlullPrev + 1, votosLlullPost);
         // fail("Fallo forzado.");
     }
 }
