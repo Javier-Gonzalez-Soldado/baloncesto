7c1014e6d07af701ace8f32b8f82cb71576b1ba2
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-20 18:55:18 +0100

    pruebaa5

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 
 public class ModeloDatosTest {
+
+    @Rule
+    public final EnvironmentVariables environmentVariables
+      = new EnvironmentVariables();
 
     @Test
     public void testExisteJugador() {
@@ -19,6 +24,14 @@ public class ModeloDatosTest {
     @Test
     public void testActualizarJugador() {
         System.out.println("Prueba de actualizarJugador");
+
+        //setear variables de entorno
+        environmentVariables.set("DATABASE_HOST", "jdbc:mysql://localhost");
+        environmentVariables.set("DATABASE_PORT", "3306");
+        environmentVariables.set("DATABASE_NAME", "baloncesto");
+        environmentVariables.set("DATABASE_USER", "usuario");
+        environmentVariables.set("DATABASE_PASS", "clave");
+
         ModeloDatos instance = new ModeloDatos();
         instance.abrirConexion();
 
