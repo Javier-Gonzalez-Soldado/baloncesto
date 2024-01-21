32bafa2811b1c256ab389cdf0a4b6aaad472cf84
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-21 17:47:14 +0100

    probando la PF-A

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -28,31 +28,31 @@ public class PruebasPhantomjsIT {
         driver.quit();
     }
 
-    // @Test
-    // public void votosACeroYVerVotosTest() {
-    //     DesiredCapabilities caps = new DesiredCapabilities();
-    //     caps.setJavascriptEnabled(true);
-    //     caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
-    //     caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
-    //             new String[] { "--web-security=no", "--ignore-ssl-errors=yes" });
-    //     driver = new PhantomJSDriver(caps);
-    //     driver.navigate().to("http://localhost:8080/Baloncesto/");
+    @Test
+    public void votosACeroYVerVotosTest() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
+                new String[] { "--web-security=no", "--ignore-ssl-errors=yes" });
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
 
-    //     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    //     driver.findElement(By.id("votosACero")).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.findElement(By.id("votosACero")).click();
 
-    //     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    //     driver.findElement(By.id("verVotos")).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.findElement(By.id("verVotos")).click();
 
-    //     WebElement tabla = driver.findElement(By.id("tablaVotos"));
-    //     List<WebElement> filas = tabla.findElements(By.tagName("tr"));
-    //     for (WebElement fila : filas) {
-    //         WebElement celdaDelVoto = fila.findElements(By.tagName("td")).get(1);
-    //         assertEquals("0", celdaDelVoto.getText(),
-    //                 "El numero de votos no es correcto");
-    //     }
+        WebElement tabla = driver.findElement(By.id("tablaVotos"));
+        List<WebElement> filas = tabla.findElements(By.tagName("tr"));
+        for (WebElement fila : filas) {
+            WebElement celdaDelVoto = fila.findElements(By.tagName("td")).get(1);
+            assertEquals("0", celdaDelVoto.getText(),
+                    "El numero de votos no es correcto");
+        }
 
-    //     driver.close();
-    //     driver.quit();
-    // }
+        driver.close();
+        driver.quit();
+    }
 }
