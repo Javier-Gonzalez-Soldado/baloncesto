bd02f2eaec957fe40e390e26c46baa93fd46f680
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-21 14:06:06 +0100

    Solucionando unos errores2

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,24 +1,20 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ModeloDatosTest {
 
-    @BeforeAll
-    public static void init() {
-        System.out.println("Configurando las variables para la base de datos...");
+
+    @Test
+    public void testExisteJugador() {
+        System.out.println("Prueba de existeJugador");
+
         //setear variables de entorno para los tests
         System.setProperty("DATABASE_HOST", "jdbc:mysql://localhost");
         System.setProperty("DATABASE_PORT", "3306");
         System.setProperty("DATABASE_NAME", "baloncesto");
         System.setProperty("DATABASE_USER", "usuario");
         System.setProperty("DATABASE_PASS", "clave");
-    }
 
-
-    @Test
-    public void testExisteJugador() {
-        System.out.println("Prueba de existeJugador");
         String nombre = "Rudy";
         ModeloDatos instance = new ModeloDatos();
         instance.abrirConexion();
@@ -32,6 +28,12 @@ public class ModeloDatosTest {
     public void testActualizarJugador() {
         System.out.println("Prueba de actualizarJugador");
 
+        //setear variables de entorno para los tests
+        System.setProperty("DATABASE_HOST", "jdbc:mysql://localhost");
+        System.setProperty("DATABASE_PORT", "3306");
+        System.setProperty("DATABASE_NAME", "baloncesto");
+        System.setProperty("DATABASE_USER", "usuario");
+        System.setProperty("DATABASE_PASS", "clave");
 
         ModeloDatos instance = new ModeloDatos();
         instance.abrirConexion();
