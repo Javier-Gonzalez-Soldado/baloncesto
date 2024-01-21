acfaa02bb8c8c76e1e440b59e3e378ca1b6f50b4
Botij0 <javier.gonzalezsolda@edu.uah.es>
2024-01-21 18:37:16 +0100

    Desarrollando PF-B

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -38,9 +38,11 @@ public class PruebasPhantomjsIT {
         driver = new PhantomJSDriver(caps);
         driver.navigate().to("http://localhost:8080/Baloncesto/");
 
+        // Pulsa el botón de poner votos a cero
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.id("votosACero")).click();
 
+        // Pulsa el botón de ver votos
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.id("verVotos")).click();
 
@@ -56,4 +58,39 @@ public class PruebasPhantomjsIT {
         driver.close();
         driver.quit();
     }
+
+    @Test
+    public void votarOtroYComprobarVotosTest() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
+                new String[] { "--web-security=no", "--ignore-ssl-errors=yes" });
+        driver = new PhantomJSDriver(caps);
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+
+        //Introducimos el nombre del jugador nuevo
+        driver.findElement(By.name("txtOtros")).sendKeys("Pepe");
+
+        //Seleccionamos el radio button de Otros
+        driver.findElement(By.id("rdbtnOtros")).click();
+
+        //Pulsamos el boton de votar
+        driver.findElement(By.name("btnVotar")).click();
+
+        //Volvemos a la página principal
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+
+        //Pulsamos el boton de ver votos
+        driver.findElement(By.id("verVotos")).click();
+
+        // Encuentra la celda correspondiente al jugador "Pepe" en la columna de votos
+        WebElement celdaPepe = driver.findElement(By.xpath("//table[@id='tablaVotos']//td[text()='Llull']/following-sibling::td"));
+
+        //Comparamo el valor de la celda con el valor esperado
+        assertEquals("1", celdaPepe.getText(), "El numero de votos no es correcto");
+
+        driver.close();
+        driver.quit();
+
 }
